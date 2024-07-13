ae9f57a18c06a5c84f0240b4a383376c79f5cddc
loof <ypeissard@gmail.com>
2024-07-14 00:29:11 +0200

    Working on navigation menu animation

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,26 @@
 import styles from "./Header.module.css"
 import Link from "next/link";
 
-export default function Header({ children }) {
+export default function Header({children}) {
     return (
         <header id="header">
             <nav>
 
                 <div className="buttons">
-                    <button id="nav-hamburger" onClick={() => {document.getElementById("header").classList.add("menu-expanded");}} aria-haspopup="true" aria-expanded="false"><span aria-hidden="true"><object
+                    <button id="nav-hamburger" onClick={() => {
+                        document.getElementById("header").classList.add("menu-expanded");
+                    }} aria-haspopup="true" aria-expanded="false"><span aria-hidden="true"><object
                         id="menu-icon" type="image/svg+xml" data="/menu.svg" title="Menu Icon">Menu</object>
                     </span></button>
-                    <button id="nav-x" onClick={() => {document.getElementById("header").classList.remove("menu-expanded");}} aria-haspopup="true" aria-expanded="true"><span aria-hidden="true"></span>
-                        <object
-                            id="close-menu-icon" type="image/svg+xml" data="/close.svg"
-                            title="Close Meue Icon">Close
+                    <button id="nav-x" onClick={() => {
+                        document.getElementById("header").classList.remove("menu-expanded");
+                    }} aria-haspopup="true" aria-expanded="true"><span
+                        aria-hidden="true"> <object
+                        id="close-menu-icon" type="image/svg+xml" data="/close.svg"
+                        title="Close Meue Icon">Close
                             Menu
-                        </object>
+                        </object></span>
+
                     </button>
                 </div>
                 <Link id="nav-home" href="/">
@@ -26,7 +31,7 @@ export default function Header({ children }) {
                     <ul>
                         <li><Link href="/" className="active">Nächste Röstung</Link></li>
                         <li><Link href="/profile">Profil</Link></li>
-                        <li><Link href="/logoutl">Logout</Link></li>
+                        <li><Link href="/logout">Logout</Link></li>
                     </ul>
                 </div>
 
